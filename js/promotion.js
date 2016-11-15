34ea71f16f4590bb8852e9ba3fbfa30d1dc214ec
caojiayan <136665305@qq.com>
2016-11-15 15:12:33 +0800

    添加了“促销管理”部分
    第十三次提交

---
+++ js/promotion.js
@@ -0,0 +1,438 @@
+//所有下拉框
+            $(function(){
+				//当列表项被点击时，把列表项的值放在输入框里面，
+				$(".dropdown").delegate("li", "click", function(e){
+					var v = $(this).attr("data-value"),
+						drop = $(this).closest(".dropdown");
+					drop.attr("data-value", v);
+					drop.find(".editor").val(v);
+					$(this).parent().hide();
+					e.stopPropagation();
+				}).delegate(".trigger", "click", function(e){    //当下拉按钮被点击时，显示数据列表
+					$(this).closest(".dropdown").find("ul").show();
+					e.stopPropagation();  //阻止冒泡，因为冒泡到 document 的时候，会隐藏列表
+				}).delegate(".search input", "click", function(e){
+					e.stopPropagation(); 
+				}).delegate(".search input", "keyup", function(e){    //当检测到列表中的输入框的时候，启动过滤，不满足的项隐藏
+					var v = $.trim(this.value), list = $(this).closest("ul").children("li");
+					if(v == "") {    //特殊情况，过滤输入框中没有值的时候迭代所有的列表项并显示它们
+						list.each(function(){
+							if(this.className.indexOf("search") != -1) {return;}//不考虑过滤输入框所在的列表项
+							$(this).text(this.innerText || this.textContent).show();
+						});
+					} else {
+						list.each(function(){   //迭代列表，
+							if(this.className.indexOf("search") != -1) {return;}  //不考虑过滤输入框所在的列表项
+							var lv = $(this).text();   //列表的文本
+							if(lv.indexOf(v) === -1) {   //不匹配过滤文本，就隐藏
+								$(this).hide();
+							} else {                 //匹配，把匹配的文本替换会含有标记的文本（红色）
+								$(this).html(lv.replace(v, '<span class="hot">'+v+'</span>')).show();
+							}
+						});
+					}
+					
+				});
+				
+				$(this).click(function(){   //当数据列表在显示时，如果点击了页面其它区域，则隐藏列表
+					$(".dropdown ul:visible").hide();   
+				});
+				//加载数据，数据列表中的每一项是一个列表，为了模拟select，每个列表项目还包括一个自定义的属性 data-value,用于保存此列表项的值
+				$("#load").click(function(){
+					var p = $(".dropdown ul");
+					var fragment = document.createDocumentFragment(), li;  //临时容器，为了避免多次添加引起页面高频率重绘
+					for(var i=1, len=30;i<len;i++) {
+						li = document.createElement("li");
+						li.setAttribute("data-value", i);
+						li.innerHTML = i;
+						fragment.appendChild(li);
+						//$('<li data-value="' + i + '">' + i + '</li>').appendTo(fragment);
+					}
+					p.append(fragment);  //将临时容器添加到无序列表里面，一次添加效率高；
+				});
+			});
+
+//选项卡；
+$(function(){
+			$('.listtr td').on('click',function(){
+				$('.listtr td').removeClass('active');
+				$(this).addClass('active');
+				$('.promoMain .box').removeClass('active');
+				$('.promoMain .box').eq($(this).index()).addClass('active');
+			});
+		});
+
+//折扣；
+//点击新增按钮，显示折扣盒子的内容；
+$(document).ready(function(){
+//	$(".dBox3Table tr td:last-child a").addClass(".PromoWrite");
+	$(".PromoBut").click(function(){     //新增；
+		$(".discBox").show();
+	});
+	$(".PromoWrite").click(function(){    //修改；
+		$(".discBox").show();
+	});
+	$(".Promoremove").click(function(){   //关闭；；
+		$(".discBox").hide(); 
+	});
+	$(".Promoyes").click(function(){      //保存；
+		$(".discBox").hide();
+		alert("已保存数据");              
+	});
+	//折扣管理修改
+	$("#discountW1").click(function(){    //修改；
+		$(".discBox").show();
+	});
+	$("#discountW2").click(function(){    //修改；
+		$(".discBox").show();
+	});
+	$("#discountW3").click(function(){    //修改；
+		$(".discBox").show();
+	});
+	$("#discountW4").click(function(){    //修改；
+		$(".discBox").show();
+	});
+	$("#discountW5").click(function(){    //修改；
+		$(".discBox").show();
+	});
+	$("#discountW6").click(function(){    //修改；
+		$(".discBox").show();
+	});
+	$("#discountW7").click(function(){    //修改；
+		$(".discBox").show();
+	});
+	$("#discountW8").click(function(){    //修改；
+		$(".discBox").show();
+	});
+});
+
+//点击按钮，展开下拉列表；
+$(document).ready(function(){
+	$(".dBox2boxOpen1").click(function(){
+		$("#boxList1").toggle();
+		$(".dBox2boxOpen1>img").toggle(); 
+	});
+	$(".dBox2boxOpen6").click(function(){
+		$("#boxList6").toggle();
+		$(".dBox2boxOpen6>img").toggle();
+	});
+	$(".dBox2boxOpen2").click(function(){
+		$("#boxList2").toggle();
+		$(".dBox2boxOpen2>img").toggle();
+	});
+	$(".dBox2boxOpen3").click(function(){
+		$("#boxList3").toggle();
+		$(".dBox2boxOpen3>img").toggle();
+	});
+	$(".dBox2boxOpen4").click(function(){
+		$("#boxList4").toggle();
+		$(".dBox2boxOpen4>img").toggle();
+	});
+	$(".dBox2boxOpen5").click(function(){
+		$("#boxList5").toggle();
+		$(".dBox2boxOpen5>img").toggle();
+	});
+});
+//折扣管理部分；
+//点击打钩；
+$(document).ready(function(){
+    $('.dBox3Ulbutton').click(function(){//给id为btn的元素添加点击事件；
+    $(this).toggleClass('active');//每次点击的时候，将当前的元素切换active样式；
+    });                              //如果有，则去掉，否则添加；
+});
+
+
+//优惠券；
+//点击新增按钮，显示优惠券盒子的内容；
+$(document).ready(function(){
+	$(".CouponBtn").click(function(){   //新增；
+		$(".coupBox").show();
+	});
+	$(".Coupremove").click(function(){  //关闭；
+		$(".coupBox").hide();
+	});
+	$(".Coupyes").click(function(){   //保存；
+		$(".coupBox").hide();
+		alert("已保存数据");
+	});
+	//优惠券管理修改
+	$("#couponW1").click(function(){   //新增；
+		$(".coupBox").show();
+	});
+	$("#couponW2").click(function(){   //新增；
+		$(".coupBox").show();
+	});
+	$("#couponW3").click(function(){   //新增；
+		$(".coupBox").show();
+	});
+	$("#couponW4").click(function(){   //新增；
+		$(".coupBox").show();
+	});
+	$("#couponW5").click(function(){   //新增；
+		$(".coupBox").show();
+	});
+	$("#couponW6").click(function(){   //新增；
+		$(".coupBox").show();
+	});
+	$("#couponW7").click(function(){   //新增；
+		$(".coupBox").show();
+	});
+	$("#couponW8").click(function(){   //新增；
+		$(".coupBox").show();
+	});
+});
+
+//满X优惠；
+//点击新增按钮，显示满X优惠盒子的内容；
+$(document).ready(function(){
+	$(".reachBtn").click(function(){
+		$(".reachBox").show();
+	});
+	$(".reachremove").click(function(){
+		$(".reachBox").hide();
+	});
+	$(".reachyes").click(function(){
+		$(".reachBox").hide();
+		alert("已保存数据");
+	});
+	//满X优惠管理修改
+	$("#favourW1").click(function(){
+		$(".reachBox").show();
+	});
+	$("#favourW2").click(function(){
+		$(".reachBox").show();
+	});
+	$("#favourW3").click(function(){
+		$(".reachBox").show();
+	});
+	$("#favourW4").click(function(){
+		$(".reachBox").show();
+	});
+	$("#favourW5").click(function(){
+		$(".reachBox").show();
+	});
+	$("#favourW6").click(function(){
+		$(".reachBox").show();
+	});
+	$("#favourW7").click(function(){
+		$(".reachBox").show();
+	});
+});
+//满X优惠管理；
+$(document).ready(function(){
+	$(".rBox3Ulboxtwo").css("display","none");  //隐藏
+	$(".rBox3Ulinputtwo").click(function(){
+		$(".rBox3Ulboxtwo").toggle();
+		$(".rBox3Ulinputtwo>img").toggle();
+		$(".rBox3Ulboxtwo").css("background","rgb(229,239,202)")
+	});
+});
+
+
+//赠送；
+//点击新增按钮，显示赠送盒子的内容；
+$(document).ready(function(){
+	$(".giveBtu").click(function(){      //新增
+		$(".giveBox").show();         
+	});
+	$(".giveremove").click(function(){   //关闭
+		$(".giveBox").hide();
+	});
+	$(".giveyes").click(function(){      //保存
+		$(".giveBox").hide();
+		alert("已保存数据");
+	});
+	//赠送管理修改
+	$("#giveW1").click(function(){      //新增
+		$(".giveBox").show();         
+	});
+	$("#giveW2").click(function(){      //新增
+		$(".giveBox").show();         
+	});
+	$("#giveW3").click(function(){      //新增
+		$(".giveBox").show();         
+	});
+	$("#giveW4").click(function(){      //新增
+		$(".giveBox").show();         
+	});
+	$("#giveW5").click(function(){      //新增
+		$(".giveBox").show();         
+	});
+	$("#giveW6").click(function(){      //新增
+		$(".giveBox").show();         
+	});
+	$("#giveW7").click(function(){      //新增
+		$(".giveBox").show();         
+	});
+});
+
+//赠送管理；
+$(document).ready(function(){
+	$(".gBox3Ulboxone").css("display","none"); //隐藏
+	$(".gBox3Ulinput").click(function(){
+		$(".gBox3Ulboxone").toggle();
+		$(".gBox3Ulinput>img").toggle();
+		$(".gBox3Ulboxone").css("background","rgb(229,239,202)")
+	});
+});
+
+
+//红包；
+//新增红包；
+$(function(){
+	$(".rpacketClick").click(function(){  //新增
+		$(".redpacketBox").show();
+	});
+	$(".rpacketno").click(function(){    //关闭
+		$(".redpacketBox").hide();
+	});
+	$(".rpacketyes").click(function(){   //保存
+		$(".redpacketBox").hide();
+		alert("已保存数据");
+	});
+	//红包管理修改
+	$("#redpacketW1").click(function(){ 
+		$(".redpacketBox").show();
+	});
+	$("#redpacketW2").click(function(){  
+		$(".redpacketBox").show();
+	});
+	$("#redpacketW3").click(function(){  
+		$(".redpacketBox").show();
+	});
+	$("#redpacketW4").click(function(){  
+		$(".redpacketBox").show();
+	});
+	$("#redpacketW5").click(function(){ 
+		$(".redpacketBox").show();
+	});
+	$("#redpacketW6").click(function(){  
+		$(".redpacketBox").show();
+	});
+});
+
+//管理列表中的全选与取消全选
+(function($){
+    $.fn.checkall = function(options){
+        var defaults = {chname:"checkname[]", callback:function(){}},
+        options = $.extend(defaults, options),
+        $obj = $(this),
+        $items = $("input[name='"+options.chname+"']"),
+        checkedItem = 0;
+        $items.click(function(){
+            if($items.filter(":checked").length === $items.length){
+                $obj.attr("checked",true);
+            }else{
+                $obj.removeAttr("checked");
+            }
+            checkedItem = $items.filter(":checked").length;
+            if(typeof options.callback === 'function') options.callback(checkedItem);
+        });
+        return $obj.each(function(){
+            $(this).click(function(){
+                if($(this).attr('checked')){
+                    $items.attr("checked",true);
+                }else{
+                    $items.removeAttr("checked");
+                }
+                checkedItem = $items.filter(":checked").length;
+                if(typeof options.callback === 'function') options.callback(checkedItem);
+            });
+        });
+    }
+})(jQuery);
+//各管理input的全选
+$(function(){
+    $("#checkall1").checkall({chname:"test1[]", callback: function(e){}});
+});
+$(function(){
+    $("#checkall2").checkall({chname:"test2[]", callback: function(e){}});
+});
+$(function(){
+    $("#checkall3").checkall({chname:"test3[]", callback: function(e){}});
+});
+$(function(){
+    $("#checkall4").checkall({chname:"test4[]", callback: function(e){}});
+});
+$(function(){
+    $("#checkall5").checkall({chname:"test5[]", callback: function(e){}});
+});
+//点击删除
+$(function(){
+	$("#delBtn1").click(function () {
+        $("#table1").find("input[name='test1[]']").each(function () {
+            if($(this).prop("checked")) {
+               $(this).parent().parent().remove();
+            }
+        });
+    });
+});
+$(function(){
+	$("#delBtn2").click(function () {
+        $("#table2").find("input[name='test2[]']").each(function () {
+            if($(this).prop("checked")) {
+               $(this).parent().parent().remove();
+            }
+        });
+    });
+});
+$(function(){
+	$("#delBtn3").click(function () {
+        $("#table3").find("input[name='test3[]']").each(function () {
+            if($(this).prop("checked")) {
+               $(this).parent().parent().remove();
+            }
+        });
+    });
+});
+$(function(){
+	$("#delBtn4").click(function () {
+        $("#table4").find("input[name='test4[]']").each(function () {
+            if($(this).prop("checked")) {
+               $(this).parent().parent().remove();
+            }
+        });
+    });
+});
+$(function(){
+	$("#delBtn5").click(function () {
+        $("#table5").find("input[name='test5[]']").each(function () {
+            if($(this).prop("checked")) {
+               $(this).parent().parent().remove();
+            }
+        });
+    });
+});
+
+//折扣工具的批量打折部分input全选
+$(function(){
+    $("#checkalla").checkall({chname:"testa[]", callback: function(e){}});
+});
+//$(function(){
+//  //全选按钮被选中的时候
+//  $("#checkalla").click(function(){
+//  //列表隐藏中的input也要被选中
+//      if ($("#checkalla").prop("checked",true)) {
+//      	$("#boxList1 input").prop("checked",true);
+//      } 
+//  })
+//});
+
+$(function(){
+    $("#checkalla1").checkall({chname:"testa1[]", callback: function(e){}});
+});
+$(function(){
+    $("#checkalla2").checkall({chname:"testa2[]", callback: function(e){}});
+});
+$(function(){
+    $("#checkalla3").checkall({chname:"testa3[]", callback: function(e){}});
+});
+$(function(){
+    $("#checkalla4").checkall({chname:"testa4[]", callback: function(e){}});
+});
+$(function(){
+    $("#checkalla5").checkall({chname:"testa5[]", callback: function(e){}});
+});
+$(function(){
+    $("#checkalla6").checkall({chname:"testa6[]", callback: function(e){}});
+});
