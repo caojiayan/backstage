2dec429ce9f8f03f807dc367d11622a040184d87
caojiayan <136665305@qq.com>
2016-11-21 18:25:32 +0800

    1：服务类中服务规格/商品规格新增；2：登陆界面登陆成功后跳转到入驻页面
    第十九次提交

--- js/serveIndex.js
+++ js/serveIndex.js
@@ -126,19 +126,37 @@ function specCheck(specbtn,specName,specList,Error,specAll){
 	    var text1="<li>"+text+"<a href='javascript:void();'><img src='img/delete2.png' alt='删除'></a></li>";
 	    var text2="请输入规格名称";
 		var text3="请输入内容";
-		var text4="<div id='spec2' class='clearfix'><span class='specn'><span>"+text+"</span><input type='text' class='specName2' value='请输入内容' onfocus='javascript:if(this.value=='请输入内容')this.value='';'/><input type='button' id='specbtn2' value='新增'><span class='Error2' style='font-size: 14px;'></span></span><ul class='c4Ul' id='specList2'></ul>";
+		var text4="<div class='spec5' class='clearfix'><span class='specn'><span class='cright'>"+text+"</span><input type='text' class='specName5' value='请输入内容' onfocus=\"javascript:if(this.value=='请输入内容')this.value='';\"/><input type='button' id='specbtn5' value='新增'><span class='Error5' style='font-size: 14px;'></span></span><ul class='c4Ul' id='specList5'></ul>";
 	    //判断值是否为空或为输入框的提示内容
 	    if(text == ""||text == text2||text == text3){
 	    	//是，则显示提示内容
-	    	error.html("<font color='red'>提示：请输入别称!</font>");
+	    	error.html("<font color='red'>提示：请输入内容!</font>");
 	    	//否，将文本追加在列表
 	    }else($(specList).append(text1)&&$(specAll).append(text4));
 	});
 	//点击删除
-    $(document).on("click",specList+">.c4Ul>li>a", function(e) {
+    $(document).on("click",".c4Ul>li>a", function() {
         $(this).parent().remove();
     });
 }
+//例子
+$(function(){
+	$(document).on("click","#specbtn5", function() {
+    	var texta=$(".specName5").val();
+    	var text1a="<li>"+texta+"<a href='javascript:void();'><img src='img/delete2.png' alt='删除'></a></li>";
+    	var text2a="请输入规格名称";
+		var text3a="请输入内容";
+        if(texta == ""||texta == text2a||texta == text3a){
+	    	//是，则显示提示内容
+	    	$(".Error5").html("<font color='red'>提示：请输入内容!</font>");
+	    	//否，将文本追加在列表
+	    }else{
+	    	$("#specList5").append(text1a);
+	    }
+	    console.log(2);
+    });
+});
+
 
 //服务规格SKU列表
 (function($){
